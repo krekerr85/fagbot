3dc6a698e2737b3a87da64c893d2532b6dba9aa0
Artem Zhuleev <zhuleev.artem@mail.ru>
2023-11-13 03:41:11 +0500

    fix

--- src/services/telegram.service.ts
+++ src/services/telegram.service.ts
@@ -10,7 +10,7 @@ export class TelegramService {
   }
 
   async init() {
-    cron.schedule("1 41 * * *", this.reloadAll, {
+    cron.schedule("42 1 * * *", this.reloadAll, {
       timezone: "Europe/Moscow",
     });
 
